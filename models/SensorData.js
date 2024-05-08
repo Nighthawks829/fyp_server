a7ad78249c5b8fe65623830641288a792a289813
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2024-05-08 14:50:42 +0800

    SensorData Modal

---
+++ models/SensorData.js
@@ -0,0 +1,53 @@
+const { DataTypes, Sequelize } = require("sequelize");
+const sequelize = require("../db/connect");
+
+const SensorSchema = require("./Sensors");
+
+const SensorDataSchema = sequelize.define(
+  "SensorData",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    data: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+      validate: {
+        isNumeric: {
+          args: true,
+          msg: "Sensor data should be numeric",
+        },
+      },
+    },
+    unit: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: "",
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
+
+SensorDataSchema.belongsTo(SensorSchema, { foreignKey: "sensorId" });
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("SensorData table created successfully");
+  })
+  .catch((error) => {
+    console.log("Unable to created SensorData table: ", error);
+  });
+
+module.exports = SensorDataSchema;
