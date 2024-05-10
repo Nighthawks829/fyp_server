ea2ab7f2a465144c7c7443d5f1d17047ff564917
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2024-05-10 14:26:46 +0800

    App Server

---
+++ app.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const express = require("express");
+const app = express();
+
+const sequelize = require("./db/connect");
+
+// import middleware
+
+// import routes
+
+// import error handler
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+// Test API
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+// routes
+
+// middleware
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await sequelize
+      .authenticate()
+      .then(() => console.log("Connection has been established"));
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
