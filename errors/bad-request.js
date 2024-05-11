bc34b55122bdea081133fa6a8565fe8fa40cae56
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2024-05-11 14:51:09 +0800

    Error Handler File

---
+++ errors/bad-request.js
@@ -0,0 +1,12 @@
+const { StatusCodes } = require("http-status-codes");
+const CustomAPIError = require("./custom-api");
+const e = require("cors");
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+module.exports = BadRequestError;
