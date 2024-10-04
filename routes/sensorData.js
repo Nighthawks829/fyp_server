578c64da0e078503667a04cd49d8e59288245bf8
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2024-10-04 11:59:58 +0800

    Update sensor data route to authenticate the user

--- routes/sensorData.js
+++ routes/sensorData.js
@@ -8,8 +8,13 @@ const {
   getLatestSensorData
 } = require("../controllers/sensorData");
 
-router.route("/").get(getAllSensorData).post(addSensorData);
-router.route("/:id").get(getSensorData);
-router.route("/latest/:id").get(getLatestSensorData);
+const authenticateUser = require("../middleware/authenticationUser");
+
+router
+  .route("/")
+  .get(authenticateUser, getAllSensorData)
+  .post(authenticateUser, addSensorData);
+router.route("/:id").get(authenticateUser, getSensorData);
+router.route("/latest/:id").get(authenticateUser, getLatestSensorData);
 
 module.exports = router;
