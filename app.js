711ecfaf72c0915459886730560c6c3518112172
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2025-02-08 10:24:08 +0800

    Add commnet to the app.js file

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const createServer = require("./utils/server");
 
 const sequelize = require("./db/connect");
+
+// Define the port number from environment variables, defaulting to 5000 if not set
 const port = process.env.PORT || 5000;
 
 const app = createServer();
 
+// Start the server and establish a database connection
 app.listen(port, async () => {
   try {
+    // Attempt to authenticate the database connection
     await sequelize
       .authenticate()
       .then(() => console.log("Connection has been establidhes"));
